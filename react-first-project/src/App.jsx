c422243f28349ec6bf53f6384f0946631b92b64c
AnugrahaTamang <tamanganugrah59@gmail.com>
2024-10-29 16:13:12 +0545

    react first project

---
+++ react-first-project/src/App.jsx
@@ -0,0 +1,14 @@
+import Add from "./Component/Add";
+import Greet from "./Component/Greet";
+
+function App() {
+  return (
+    <>
+     <h1>I AM ANUGRAHA TAMANG FROM DHADING NEPAL.</h1>
+    <Greet/>
+    <Add/>
+    </>
+  )
+}
+
+export default App
---
+++ react-first-project/src/Component/Greet.jsx
@@ -0,0 +1,6 @@
+function Greet(){
+    return(
+        <h1>I am from Component</h1>
+    )
+}
+export default Greet;
