90f4d643a4d64694b9640320362f0bf175647221
AnugrahaTamang <tamanganugrah59@gmail.com>
2024-10-29 18:26:17 +0545

    jsx in react

---
+++ jsx-in-react/src/App.jsx
@@ -0,0 +1,11 @@
+import JsxRull from "./JsxRule/JsxRule"
+
+function App() {
+  return (
+    <>
+      <JsxRull/>
+   </>
+  )
+}
+
+export default App
---
+++ jsx-in-react/src/JsxRule/JsxRule.jsx
@@ -0,0 +1,15 @@
+function JsxRull(){
+    return(
+        <>
+        <p>Only return one div or one fragment</p>
+        <p className="one">when you declare the class you should to written className</p>
+        <p id="bestid">Id declaration</p>
+        <p>when you create the form you have to noticed: form not support the for but support htmlfor</p>
+        <form action="">
+            <label htmlFor="name">Enter your Name:</label>
+            <input type="text" placeholder="Enter your Name" />
+        </form>
+        </>
+    )
+}
+export default JsxRull;
